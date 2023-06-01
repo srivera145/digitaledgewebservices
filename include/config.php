7d47fc18195362d4d8f893d4345c51e25b3b6661
Santos Rivera <srivera145@gmail.com>
2023-06-01 14:36:19 -0400

    update db

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "https://digitaledgewebservices.herokuapp.com";
-$db_name = "portfolio";
-$db_pass = "";
-$db_user = "root";
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_name = "heroku_a967d42b92a888a ";
+$db_pass = "6e631e0a";
+$db_user = "b5469bbfcda832";
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
