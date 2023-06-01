d75d55762e7b4aefd6f0b4106218c232b93036b2
Santos Rivera <srivera145@gmail.com>
2023-06-01 15:02:34 -0400

    more update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "us-cdbr-east-06.cleardb.net";
-$db_name = "heroku_a967d42b92a888a ";
-$db_pass = "6e631e0a";
-$db_user = "b5469bbfcda832";
+$db_host = "6u16br.stackhero-network.com";
+$db_name = "portfolio";
+$db_pass = "XyiOd0tvJr7JjWNuECai5kbBtLhVOCw4";
+$db_user = "root";
 
 $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Could not connect to database!");
 
